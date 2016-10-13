479fb3ef69c25881a964153ab11489aae8c39a93
Amin mnasour <ama>
2016-10-13 20:17:45 +0100

    I have finished part a

--- node.h
+++ node.h
@@ -9,22 +9,34 @@ using std::endl;
 // TODO your code for the Node class goes here:
 // (Note the 'NodeIterator' class will report lots of errors until Node exists
 
+template<typename T>
+class Node{
+public:
+  T data;
+  Node<T> *next;
+  Node<T> *prev;
+
+  Node(T *dataInput,Node *nextNode,Node *prevNode)
+      : date(dataInput),next(nextNode),prev(prevNode){
+  }
+
+}
 
 
 
 
 template<typename T>
 class NodeIterator {
-  
+
 private:
-    
+
     Node<T>* current;
-    
+
 public:
-    
+
 
     NodeIterator(Node<T>* currentIn)
-        : current(currentIn) {        
+        : current(currentIn) {
     }
 
     T & operator*() {
@@ -32,8 +44,8 @@ public:
     }
 
     // TODO: complete the code for NodeIterator here
-        
-    
+
+
 };
 
 // do not edit below this line
